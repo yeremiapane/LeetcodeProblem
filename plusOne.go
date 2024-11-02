091f4d92dc0a9592db1bc0709cd1a94cc313f496
yerem <yeremiayosefan@mhs.unimed.ac.id>
2024-11-02 20:14:32 +0700

    last update

--- plusOne.go
+++ plusOne.go
@@ -1,13 +1,15 @@
 package main
 
+import "fmt"
+
 func plusOne(digits []int) []int {
 	total := []int{}
 	for i := 0; i < len(digits); i++ {
 		total = append(total, i)
-
 	}
+	return total
 }
 
 func main() {
-
+	fmt.Println(plusOne([]int{1, 2, 3}))
 }
