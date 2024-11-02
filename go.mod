16ad98a73141c0fd00f48b5980bfadb978faff4f
yerem <yeremiayosefan@mhs.unimed.ac.id>
2024-11-02 23:28:20 +0700

    last update

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module LeetCodeProblems
+
+go 1.22
--- plusOne.go
+++ plusOne.go
@@ -1,15 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func plusOne(digits []int) []int {
-	total := []int{}
-	for i := 0; i < len(digits); i++ {
-		total = append(total, i)
+	n := len(digits)
+	digits[n-1] += 1
+
+	for i := n - 1; i > 0; i-- {
+		if digits[i] == 10 {
+			digits[i] = 0
+			digits[i-1] += 1
+		}
 	}
-	return total
+
+	if digits[0] == 10 {
+		digits[0] = 0
+		digits = append([]int{1}, digits...)
+	}
+
+	return digits
 }
 
 func main() {
 	fmt.Println(plusOne([]int{1, 2, 3}))
+	fmt.Println(plusOne([]int{9}))
+	fmt.Println(plusOne([]int{4, 3, 2, 1}))
+	fmt.Println(plusOne([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}))
 }
