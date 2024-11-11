8ce320552cfbe8b966869422c39249beb6e7d8c0
yerem <yeremiayosefan@mhs.unimed.ac.id>
2024-11-11 22:43:44 +0700

    11/11/2024

--- PrimeSubtractionOperation.go
+++ PrimeSubtractionOperation.go
@@ -1,3 +1,8 @@
+/*
+	Problem : 2601. Prime Subtraction Operation
+	Link : https://leetcode.com/problems/prime-subtraction-operation/description/?envType=daily-question&envId=2024-11-11
+*/
+
 package main
 
 import (
