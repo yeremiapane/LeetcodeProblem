13be9268c0584e070680aa0596ed412e7bceac74
yerem <yeremiayosefan@mhs.unimed.ac.id>
2024-11-23 22:25:41 +0700

    23/11/2024

---
+++ Prob_1861.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func rotateTheBox(box [][]byte) [][]byte {
+	m := len(box)
+	n := len(box[0])
+
+	for i := 0; i < m; i++ {
+		temp := -1
+		for j := n - 1; j >= 0; j-- {
+			if box[i][j] == '*' {
+				temp = -1
+			} else if box[i][j] == '.' && temp == -1 {
+				temp = j
+			} else if box[i][j] == '#' && temp != -1 {
+				box[i][j], box[i][temp] = box[i][temp], box[i][j]
+				temp--
+			}
+		}
+	}
+	result := make([][]byte, n)
+	for i := 0; i < n; i++ {
+		for j := m - 1; j >= 0; j-- {
+			result[i] = append(result[i], box[j][i])
+		}
+	}
+	return result
+}
+
+func main() {
+	testCases := []struct {
+		box      [][]byte
+		expected [][]byte
+	}{
+		{box: [][]byte{{'#', '.', '#'}}, expected: [][]byte{{'.'}, {'#'}, {'#'}}},
+		{box: [][]byte{{'#', '.', '*', '.'}, {'#', '#', '*', '.'}}, expected: [][]byte{{'#', '.'}, {'#', '#'}, {'*', '*'}, {'.', '.'}}},
+	}
+	for _, tC := range testCases {
+		fmt.Printf("Input Box : %v\n", tC.box)
+		fmt.Printf("Output : %v\n", rotateTheBox(tC.box))
+		fmt.Printf("Expected : %v\n", tC.expected)
+	}
+}
